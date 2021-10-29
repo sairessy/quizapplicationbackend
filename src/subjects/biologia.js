1aebf88521d7d47913fbb87618ced64b439a2697
sairessy <sairessy@gmail.com>
2021-10-29 01:48:01 -1000

    real questions

---
+++ src/subjects/biologia.js
@@ -0,0 +1,14 @@
+const biologia = [
+  {
+    id: 'biologia0',
+    text: 'A classificação dos seres vivos em cinco (5) reinos foi proposta por:',
+    answers: ['Carl Von Linné', 'Aristóteles', 'Wittaker', 'Coopeland', 'Haeckel'],
+    correct: 2,
+    subject: 4,
+    level: 11,
+    institution: 0,
+    year: 2017
+  }
+];
+
+module.exports = biologia;
